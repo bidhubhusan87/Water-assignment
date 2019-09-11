695a7031f6af5e0b339cef970925f620cfdce527
Bidhu Bhusan <bidhu.bhusan@metacube.com>
2019-09-11 09:58:14 +0530

    added test case for card compnent getcarddetails

--- src/app/cards/cards.component.spec.ts
+++ src/app/cards/cards.component.spec.ts
@@ -9,18 +9,33 @@ import { cardModel } from '../models/data.model';
 
 describe('CardsComponent', () => {
   let component: CardsComponent;
+  let propertyAfterResponse;
+  let propertExpectedAfterRespnise;
   let fixture: ComponentFixture<CardsComponent>;
+  let DummyService: GetCardDetailsService;
+  let DummyOffer: cardModel;
+  
 
-  beforeEach(async(() => {
+  beforeEach(async((DummyOffer) => {
     TestBed.configureTestingModule({
       imports: [
         MatGridListModule,
         RouterModule.forRoot([]),
         HttpClientTestingModule
       ],
-      declarations: [ CardsComponent ]
+      declarations: [ CardsComponent ],
+      providers: [GetCardDetailsService]
     })
     .compileComponents();
+    DummyOffer = {
+      id: '1',
+      type: 'STARTER',
+      price: 1,
+      detail: 'Starter features for your business to grow.',
+      selected: false,
+      category: 'tab1'
+    };
+    DummyService = TestBed.get(GetCardDetailsService);
   }));
 
   beforeEach(() => {
@@ -31,5 +46,17 @@ describe('CardsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#getOffers should return an array with Offer objects', async () => {
+    DummyService.getCardDetails().subscribe(value => {
+      propertExpectedAfterRespnise = Object.getOwnPropertyNames(value[0]);
+      propertyAfterResponse = Object.getOwnPropertyNames(DummyOffer);
+      expect(propertExpectedAfterRespnise).toEqual(propertyAfterResponse);
+    });
+  });
+
+  it('should have getCardDetails', () => {
+    expect(component.getCardDetails).toBeTruthy();
   });
 });
